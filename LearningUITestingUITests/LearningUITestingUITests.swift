d4ae259f3b8e3596ab371cc460c6ba39073232ed
Rommel Rico <me@rommelrico.com>
2017-10-10 09:25:52 -0700

    Test 3 – Ensuring the increment button increments the value label

--- LearningUITestingUITests/LearningUITestingUITests.swift
+++ LearningUITestingUITests/LearningUITestingUITests.swift
@@ -29,4 +29,16 @@ class LearningUITestingUITests: XCTestCase {
         XCTAssertEqual(app.navigationBars.element.identifier, "Detail")
     }
     
+    func testTapIncrementButtonIncrementsValueLabel() {
+        let app = XCUIApplication()
+        app.buttons["View Detail"].tap()
+        let incrementButton = app.buttons["Increment Value"]
+        let valueLabel = app.staticTexts["Number Value Label"]
+        
+        for index in 0...9 {
+            incrementButton.tap()
+            XCTAssertEqual(valueLabel.value as! String, "\(index + 1)")
+        }
+    }
+    
 }
